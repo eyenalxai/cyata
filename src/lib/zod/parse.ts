b3bd72c523cb7353f0e25d47f709b83441a424f4
Sax Dhiut <mail@iyxeyl.me>
2024-09-03 21:26:05 +0400

    feat: add zod schema parsing utility
    Introduce parseZodSchema function to validate data against ZodSchema. Utilize the neverthrow library to handle successful and erroneous parsing results. This helps in improving type safety and error management during schema validations.

---
+++ src/lib/zod/parse.ts
@@ -0,0 +1,10 @@
+import { type Result, err, ok } from "neverthrow"
+import type { ZodSchema } from "zod"
+
+export const parseZodSchema = <T>(schema: ZodSchema<T>, data: unknown): Result<T, string> => {
+	const result = schema.safeParse(data)
+
+	if (result.success) return ok(result.data)
+
+	return err(result.error.errors.map((e) => e.message).join(", "))
+}
