a9622238208dffb6ad0d9118b0ce7dc51aa82200
Sax Dhiut <mail@iyxeyl.me>
2024-09-03 21:22:32 +0400

    feat: add session management and user creation timestamps
    Introduced sessions table with user relations and added createdAt field to users table. This enables tracking user creation time and managing user sessions effectively.

--- drizzle/0000_glorious_jigsaw.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS "users" (
-	"uuid" uuid DEFAULT gen_random_uuid(),
-	"username" text NOT NULL,
-	"password_hash" text NOT NULL
-);
--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -1,8 +1,34 @@
-import { sql } from "drizzle-orm"
-import { pgTable, text, uuid } from "drizzle-orm/pg-core"
+import { relations, sql } from "drizzle-orm"
+import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core"
 
 export const users = pgTable("users", {
-	uuid: uuid("uuid").default(sql`gen_random_uuid()`),
+	uuid: uuid("uuid").default(sql`gen_random_uuid()`).primaryKey(),
+	createdAt: timestamp("created_at").default(sql`now()`),
 	username: text("username").notNull(),
 	passwordHash: text("password_hash").notNull()
 })
+
+export type User = typeof users.$inferSelect
+export type UserInsert = typeof users.$inferInsert
+
+export const usersRelations = relations(users, ({ many }) => ({
+	sessions: many(sessions)
+}))
+
+export const sessions = pgTable("sessions", {
+	key: text("text").primaryKey(),
+	expiresAt: timestamp("expires_at", { mode: "string", withTimezone: true }).notNull(),
+	userUuid: uuid("user_uuid")
+		.references(() => users.uuid, { onDelete: "cascade" })
+		.notNull()
+})
+
+export type Session = typeof sessions.$inferSelect
+export type SessionInsert = typeof sessions.$inferInsert
+
+export const sessionsRelations = relations(sessions, ({ one }) => ({
+	author: one(users, {
+		fields: [sessions.userUuid],
+		references: [users.uuid]
+	})
+}))
