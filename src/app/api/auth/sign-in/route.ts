32dbff7c1a155231d07eead781a4849d89d9073d
Sax Dhiut <mail@iyxeyl.me>
2024-09-04 18:51:26 +0400

    refactor: add transaction handling to auth routes
    Wrap sign-up and sign-in logic in database transactions to ensure atomicity. This change aims to maintain data integrity and consistency during user creation or authentication.

--- src/app/api/auth/sign-in/route.ts
+++ src/app/api/auth/sign-in/route.ts
@@ -1,5 +1,6 @@
 import { verifyPassword } from "@/lib/crypto/password"
 import { secureRandomToken } from "@/lib/crypto/token"
+import { db } from "@/lib/database/client"
 import { insertSession } from "@/lib/database/session"
 import { selectUserByUsername } from "@/lib/database/user"
 import { env } from "@/lib/env.mjs"
@@ -9,39 +10,41 @@ import { cookies } from "next/headers"
 import { NextResponse } from "next/server"
 
 export const POST = async (request: Request) => {
-	const signInDataResult = parseZodSchema(AuthFormSchema, await request.json())
+	return await db.transaction(async (trx) => {
+		const signInDataResult = parseZodSchema(AuthFormSchema, await request.json())
 
-	if (signInDataResult.isErr()) return new NextResponse(signInDataResult.error, { status: 400 })
+		if (signInDataResult.isErr()) return new NextResponse(signInDataResult.error, { status: 400 })
 
-	const userResult = await selectUserByUsername(signInDataResult.value.username)
+		const userResult = await selectUserByUsername(trx, signInDataResult.value.username)
 
-	if (userResult.isErr()) return new NextResponse(userResult.error, { status: 500 })
+		if (userResult.isErr()) return new NextResponse(userResult.error, { status: 500 })
 
-	const isPasswordValid = await verifyPassword({
-		password: signInDataResult.value.password,
-		hash: userResult.value.passwordHash
+		const isPasswordValid = await verifyPassword({
+			password: signInDataResult.value.password,
+			hash: userResult.value.passwordHash
+		})
+
+		if (!isPasswordValid) return new NextResponse("Invalid password", { status: 400 })
+
+		const insertSessionResult = await insertSession(trx, {
+			key: await secureRandomToken(),
+			expiresAt: new Date(
+				Date.now() + 1000 * 60 * 60 * 24 * env.SESSION_COOKIES_EXPIRES_IN_DAYS - 1000 * 60 * 5
+			).toISOString(), // 5 minutes before cookie expires
+			userUuid: userResult.value.uuid
+		})
+
+		if (insertSessionResult.isErr()) return new NextResponse(insertSessionResult.error, { status: 500 })
+
+		const cookieStore = cookies()
+
+		cookieStore.set(env.SESSION_COOKIE_NAME, insertSessionResult.value.key, {
+			httpOnly: true,
+			secure: process.env.NODE_ENV === "production",
+			sameSite: "strict",
+			expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * env.SESSION_COOKIES_EXPIRES_IN_DAYS)
+		})
+
+		return new NextResponse("User created", { status: 201 })
 	})
-
-	if (!isPasswordValid) return new NextResponse("Invalid password", { status: 400 })
-
-	const insertSessionResult = await insertSession({
-		key: await secureRandomToken(),
-		expiresAt: new Date(
-			Date.now() + 1000 * 60 * 60 * 24 * env.SESSION_COOKIES_EXPIRES_IN_DAYS - 1000 * 60 * 5
-		).toISOString(), // 5 minutes before cookie expires
-		userUuid: userResult.value.uuid
-	})
-
-	if (insertSessionResult.isErr()) return new NextResponse(insertSessionResult.error, { status: 500 })
-
-	const cookieStore = cookies()
-
-	cookieStore.set(env.SESSION_COOKIE_NAME, insertSessionResult.value.key, {
-		httpOnly: true,
-		secure: process.env.NODE_ENV === "production",
-		sameSite: "strict",
-		expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * env.SESSION_COOKIES_EXPIRES_IN_DAYS)
-	})
-
-	return new NextResponse("User created", { status: 201 })
 }
--- src/app/api/auth/sign-up/route.ts
+++ src/app/api/auth/sign-up/route.ts
@@ -1,5 +1,6 @@
 import { hashPassword } from "@/lib/crypto/password"
 import { secureRandomToken } from "@/lib/crypto/token"
+import { db } from "@/lib/database/client"
 import { insertSession } from "@/lib/database/session"
 import { existsUserByUsername, insertUser } from "@/lib/database/user"
 import { env } from "@/lib/env.mjs"
@@ -9,43 +10,45 @@ import { cookies } from "next/headers"
 import { NextResponse } from "next/server"
 
 export const POST = async (request: Request) => {
-	const signUpDataResult = parseZodSchema(AuthFormSchema, await request.json())
+	return await db.transaction(async (trx) => {
+		const signUpDataResult = parseZodSchema(AuthFormSchema, await request.json())
 
-	if (signUpDataResult.isErr()) return new NextResponse(signUpDataResult.error, { status: 400 })
+		if (signUpDataResult.isErr()) return new NextResponse(signUpDataResult.error, { status: 400 })
 
-	const userExistsResult = await existsUserByUsername(signUpDataResult.value.username)
+		const userExistsResult = await existsUserByUsername(trx, signUpDataResult.value.username)
 
-	if (userExistsResult.isErr()) return new NextResponse(userExistsResult.error, { status: 500 })
+		if (userExistsResult.isErr()) return new NextResponse(userExistsResult.error, { status: 500 })
 
-	if (userExistsResult.value) return new NextResponse("User already exists", { status: 400 })
+		if (userExistsResult.value) return new NextResponse("User already exists", { status: 400 })
 
-	const passwordHash = await hashPassword(signUpDataResult.value.password)
+		const passwordHash = await hashPassword(signUpDataResult.value.password)
 
-	const insertUserResult = await insertUser({
-		username: signUpDataResult.value.username,
-		passwordHash: passwordHash
+		const insertUserResult = await insertUser(trx, {
+			username: signUpDataResult.value.username,
+			passwordHash: passwordHash
+		})
+
+		if (insertUserResult.isErr()) return new NextResponse(insertUserResult.error, { status: 500 })
+
+		const insertSessionResult = await insertSession(trx, {
+			key: await secureRandomToken(),
+			expiresAt: new Date(
+				Date.now() + 1000 * 60 * 60 * 24 * env.SESSION_COOKIES_EXPIRES_IN_DAYS - 1000 * 60 * 5
+			).toISOString(), // 5 minutes before cookie expires
+			userUuid: insertUserResult.value.uuid
+		})
+
+		if (insertSessionResult.isErr()) return new NextResponse(insertSessionResult.error, { status: 500 })
+
+		const cookieStore = cookies()
+
+		cookieStore.set(env.SESSION_COOKIE_NAME, insertSessionResult.value.key, {
+			httpOnly: true,
+			secure: process.env.NODE_ENV === "production",
+			sameSite: "strict",
+			expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * env.SESSION_COOKIES_EXPIRES_IN_DAYS)
+		})
+
+		return new NextResponse("User created", { status: 201 })
 	})
-
-	if (insertUserResult.isErr()) return new NextResponse(insertUserResult.error, { status: 500 })
-
-	const insertSessionResult = await insertSession({
-		key: await secureRandomToken(),
-		expiresAt: new Date(
-			Date.now() + 1000 * 60 * 60 * 24 * env.SESSION_COOKIES_EXPIRES_IN_DAYS - 1000 * 60 * 5
-		).toISOString(), // 5 minutes before cookie expires
-		userUuid: insertUserResult.value.uuid
-	})
-
-	if (insertSessionResult.isErr()) return new NextResponse(insertSessionResult.error, { status: 500 })
-
-	const cookieStore = cookies()
-
-	cookieStore.set(env.SESSION_COOKIE_NAME, insertSessionResult.value.key, {
-		httpOnly: true,
-		secure: process.env.NODE_ENV === "production",
-		sameSite: "strict",
-		expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * env.SESSION_COOKIES_EXPIRES_IN_DAYS)
-	})
-
-	return new NextResponse("User created", { status: 201 })
 }
