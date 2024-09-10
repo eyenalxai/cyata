ded753fa36f26d526d1719c6e9789c01e74de20f
Sax Dhiut <mail@iyxeyl.me>
2024-09-10 12:42:55 +0400

    feat: add drawer and header components with sidebar integration
    Introduced new Drawer component for enhanced UI interactions. Added Header and Sidebar components, integrating the Drawer for better layout management. Updated global styles and package dependencies accordingly.

--- src/app/chat/layout.tsx
+++ src/app/chat/layout.tsx
@@ -1,6 +1,12 @@
+import { Header } from "@/components/header"
 import { cn } from "@/lib/utils"
 import type { ReactNode } from "react"
 
 export default function ChatLayout({ children }: { children: ReactNode }) {
-	return <main className={cn("container", "mx-auto", "max-w-5xl", "p-4")}>{children}</main>
+	return (
+		<>
+			<Header />
+			<main className={cn("container", "mx-auto", "max-w-5xl", "p-4")}>{children}</main>
+		</>
+	)
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,35 @@
+import { Sidebar } from "@/components/sidebar"
+import { getSession } from "@/lib/session"
+import { cn } from "@/lib/utils"
+import { redirect } from "next/navigation"
+
+export const Header = async () => {
+	const session = await getSession()
+
+	if (session.isErr()) {
+		redirect("/auth")
+	}
+
+	return (
+		<header
+			className={cn(
+				"pl-6",
+				"py-2",
+				"sticky",
+				"top-0",
+				"z-50",
+				"flex",
+				"flex-row",
+				"w-full",
+				"shrink-0",
+				"items-center",
+				"justify-start",
+				"backdrop-blur",
+				"border-b",
+				"gap-4"
+			)}
+		>
+			<Sidebar />
+		</header>
+	)
+}
