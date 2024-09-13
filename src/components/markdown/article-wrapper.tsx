3246957616752917ff4254a0348ce022484bc4a9
Sax Dhiut <mail@iyxeyl.me>
2024-09-14 00:28:31 +0400

    refactor: improve article styling
    Added custom classes to refine spacing of headings and paragraphs within articles for better readability. Introduced margin reset for preformatted text to achieve a consistent layout.

--- src/components/markdown/article-wrapper.tsx
+++ src/components/markdown/article-wrapper.tsx
@@ -8,11 +8,16 @@ type ArticleWrapperProps = {
 export const ArticleWrapper = ({ children }: ArticleWrapperProps) => {
 	return (
 		<article
-			className={cn("max-w-none", "prose", "prose-slate", "dark:prose-invert", "break-words", [
-				"prose-pre:bg-background",
-				"prose-pre:text-primary",
-				"prose-pre:p-0"
-			])}
+			className={cn(
+				"max-w-none",
+				"prose",
+				"prose-slate",
+				"dark:prose-invert",
+				"break-words",
+				["prose-pre:bg-background", "prose-pre:text-primary", "prose-pre:p-0", "prose-pre:m-0"],
+				"prose-headings:my-2",
+				"prose-p:my-0"
+			)}
 		>
 			{children}
 		</article>
