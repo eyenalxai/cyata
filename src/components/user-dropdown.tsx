3285e439c5e1d7d1a64827ff4acb522cdaccb149
Sax Dhiut <mail@iyxeyl.me>
2024-09-13 15:52:29 +0400

    feat: add revenue data display in user dropdown
    Included a new dropdown menu item to show current and previous month's revenue, along with a total revenue summary. This provides quick financial insights directly from the user dropdown.

--- src/components/user-dropdown.tsx
+++ src/components/user-dropdown.tsx
@@ -11,7 +11,7 @@ import {
 } from "@/components/ui/dropdown-menu"
 import { signOut } from "@/lib/fetch/auth"
 import { cn } from "@/lib/utils"
-import { CircleUserRound, LogOut, Settings } from "lucide-react"
+import { CircleUserRound, DollarSign, LogOut, Settings } from "lucide-react"
 import Link from "next/link"
 import { useRouter } from "next/navigation"
 import { toast } from "sonner"
@@ -38,6 +38,25 @@ export const UserDropdown = ({ username }: UserDropdownProps) => {
 						<Settings className={cn("mr-2", "size-4")} />
 						<span>Settings</span>
 					</Link>
+				</DropdownMenuItem>
+				<DropdownMenuItem className={cn("pointer-events-none")}>
+					<div className={cn("w-full", "flex", "flex-row", "justify-center", "items-start", "gap-x-2")}>
+						<DollarSign className={cn("size-4", "mt-1")} />
+						<div className={cn("flex", "flex-col", "justify-center", "items-stretch", "gap-y-1")}>
+							<div className={cn("flex", "flex-row", "justify-between", "items-center", "gap-x-2")}>
+								<span className={cn("text-xs")}>Current Month:</span>
+								<span className={cn("text-xs")}>$100.2</span>
+							</div>
+							<div className={cn("flex", "flex-row", "justify-between", "items-center", "gap-x-2")}>
+								<span className={cn("text-xs")}>Previous Month:</span>
+								<span className={cn("text-xs")}>$100.2</span>
+							</div>
+							<div className={cn("flex", "flex-row", "justify-between", "items-center", "gap-x-2")}>
+								<span className={cn("text-xs", "font-semibold")}>Total:</span>
+								<span className={cn("text-xs", "font-semibold")}>$100.2</span>
+							</div>
+						</div>
+					</div>
 				</DropdownMenuItem>
 				<DropdownMenuSeparator />
 				<DropdownMenuItem
