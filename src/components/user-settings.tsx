9262f9b15cfdaee97bea6a1ad714c5fa0bd97ca6
Sax Dhiut <mail@iyxeyl.me>
2024-09-14 10:52:24 +0400

    feat: improve textarea height in user settings
    Adjust textarea height for better user experience. Added an empty fragment in the component to resolve potential rendering issues.

--- src/components/user-settings.tsx
+++ src/components/user-settings.tsx
@@ -27,6 +27,7 @@ export const UserSettings = ({ initialUserPreferences }: ProfileProps) => {
 
 	return (
 		<div className={cn("w-full", "flex", "flex-col", "justify-center", "items-start", "gap-y-4")}>
+			{}
 			<h1 className={cn("text-2xl", "font-bold")}>Settings</h1>
 			<div className={cn("flex", "flex-col", "justify-start", "items-start", "gap-y-1")}>
 				<Label className={cn("ml-1")}>Default model</Label>
@@ -35,9 +36,7 @@ export const UserSettings = ({ initialUserPreferences }: ProfileProps) => {
 			<div className={cn("w-full", "flex", "flex-col", "justify-start", "items-start", "gap-y-1")}>
 				<Label className={cn("ml-1")}>Default System Prompt</Label>
 				<Textarea
-					className={cn("resize-none")}
-					autoResize
-					rows={Math.max(preferencesResult.value.systemPrompt.split("\n").length, 4)}
+					className={cn("resize-none", "h-64")}
 					onChange={(event) => updateSystemPrompt(event.target.value)}
 					defaultValue={preferencesResult.value.systemPrompt}
 				/>
