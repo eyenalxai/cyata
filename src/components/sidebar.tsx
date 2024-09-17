8f8e64a568b971e88490a751148c0fd33225d074
Sax Dhiut <mail@iyxeyl.me>
2024-09-17 18:26:05 +0400

    feat: add close button to sidebar
    Added a new close button to the sidebar for improved user experience. This button closes the sidebar when clicked, providing a more intuitive and efficient interface.

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button"
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
 import { cn } from "@/lib/utils"
 import type { ChatsResponse } from "@/lib/zod/api"
-import { PanelLeft } from "lucide-react"
+import { PanelLeft, X } from "lucide-react"
 import Link from "next/link"
 import { useState } from "react"
 import type { z } from "zod"
@@ -26,10 +26,13 @@ export const Sidebar = ({ initialChats }: SidebarProps) => {
 				</Button>
 			</SheetTrigger>
 			<SheetContent side={"left"} className={cn("w-full", "sm:w-96", "p-4", "overflow-y-auto")}>
-				<div className={cn("w-full", "flex", "flex-col", "justify-center", "items-start", "gap-y-2")}>
-					<div className={cn("w-full", "px-2")}>
+				<div className={cn("w-full", "flex", "flex-col", "justify-start", "items-start", "gap-y-2")}>
+					<div className={cn("w-full", "px-2", "flex", "flex-row", "justify-center", "items-center", "gap-x-2")}>
 						<Button autoFocus asChild className={cn("w-full")} onClick={() => setOpen(false)}>
 							<Link href={`/chat/${crypto.randomUUID()}`}>New Chat</Link>
+						</Button>
+						<Button variant={"ghost"} size={"icon"} className={cn("w-12")} onClick={() => setOpen(false)}>
+							<X className={cn("size-5")} />
 						</Button>
 					</div>
 					<ChatList initialChats={initialChats} className={cn("my-4")} />
