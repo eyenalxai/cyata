dd39d7d661552caa12ac0e129d31f2753ec15c31
Sax Dhiut <mail@iyxeyl.me>
2024-09-17 17:34:53 +0400

    refactor: improve script loading strategy in auth layout
    Update the script's loading strategy to 'beforeInteractive' for better performance. This ensures the script executes sooner, enhancing the overall user experience.

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -5,7 +5,11 @@ import type { ReactNode } from "react"
 export default function ChatLayout({ children }: { children: ReactNode }) {
 	return (
 		<>
-			<Script id="turnstile-script" src="https://challenges.cloudflare.com/turnstile/v0/api.js" async defer />
+			<Script
+				id="turnstile-script"
+				src="https://challenges.cloudflare.com/turnstile/v0/api.js"
+				strategy="beforeInteractive"
+			/>
 			<main className={cn("container", "mx-auto", "max-w-5xl", "p-4")}>{children}</main>
 		</>
 	)
