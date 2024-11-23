2df76abb107b0fcd723032d64cdcbbd1e1bdb617
Sax Dhiut <mail@iyxeyl.me>
2024-11-23 14:31:52 +0400

    refactor: remove unused rehype-highlight plugin
    Rehype-highlight plugin and its associated dependency have been removed. This change ensures better bundle size and performance by eliminating unnecessary code.

--- src/components/markdown/markdown-display'.tsx
+++ src/components/markdown/markdown-display'.tsx
@@ -1,10 +1,8 @@
 import { CodeComponent } from "@/components/code-block"
 import { ArticleWrapper } from "@/components/markdown/article-wrapper"
 import { isInline } from "@/lib/utils"
-import { all } from "lowlight"
 import { memo } from "react"
 import Markdown from "react-markdown"
-import rehypeHighlight from "rehype-highlight"
 import rehypeKatex from "rehype-katex"
 import rehypeSanitize from "rehype-sanitize"
 import remarkMath from "remark-math"
@@ -19,7 +17,7 @@ const MarkdownDisplay = ({ markdown }: MarkdownDisplayProps) => (
 	<ArticleWrapper>
 		<Markdown
 			remarkPlugins={[remarkParse, remarkMath, remarkRehype]}
-			rehypePlugins={[rehypeSanitize, rehypeKatex, [rehypeHighlight, { languages: all }]]}
+			rehypePlugins={[rehypeSanitize, rehypeKatex]}
 			components={{
 				code(props) {
 					if (isInline(props.children)) return <code {...props} />
